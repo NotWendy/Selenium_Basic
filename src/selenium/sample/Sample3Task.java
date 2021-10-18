59d1f3b563e1cae7751fdf30f50f6669aa7b8b85
Amiran <47554589+NotWendy@users.noreply.github.com>
2021-10-18 17:03:11 +0300

    Sample3Task done

--- src/selenium/sample/Sample3Task.java
+++ src/selenium/sample/Sample3Task.java
@@ -1,5 +1,6 @@
 package selenium.sample;
 
+import com.sun.source.tree.AssertTree;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,8 +11,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
 import java.util.List;
+import java.util.Locale;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class Sample3Task {
     WebDriver driver;
@@ -40,6 +43,10 @@ public class Sample3Task {
 //         TODO:
 //         check how many element with class "test" there are on page (5)
 //         check that value of second button is "This is also a button"
+        List<WebElement> allElementsWithClass = driver.findElements(By.className("test"));
+        System.out.println("Number of test class elements: " + allElementsWithClass.size());
+        String buttonText = driver.findElement(By.id("buttonId")).getAttribute("value");
+        assertEquals("This is also a button",buttonText);
     }
 
     @Test
@@ -48,18 +55,24 @@ public class Sample3Task {
 //         check that it is True that value of second button is
 //         "this is Also a Button" if you ignore Caps Locks
 //         fail with custom error message:
+        String buttonText = driver.findElement(By.id("buttonId")).getAttribute("value").toLowerCase(Locale.ROOT);
+        assertTrue("Expecting the text of the second button in lowercase to be 'this is also a button'",
+                buttonText.equals("unbelievable sample text"));
     }
 
     @Test
     public void assertFalseTask() throws Exception {
 //         TODO:
 //        check that it is False that value of second button is "This is a button"
+        String buttonText = driver.findElement(By.id("buttonId")).getAttribute("value");
+        assertFalse(buttonText.equals("This is a button"));
     }
 
     @Test
     public void failTask() throws Exception {
 //        TODO:
-//        check that none of items with class "test"
-//        contain number 190
+//        check that none of items with class "test" contain number 190
+        List<WebElement> list = driver.findElements(By.xpath("//*[contains(text(),'\"190\"')]"));
+        assertTrue("Text not found!", list.size() > 0);
     }
 }
