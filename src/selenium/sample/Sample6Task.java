a827228dffe4a16027cb56fb526021047463f809
Amiran <47554589+NotWendy@users.noreply.github.com>
2021-10-19 14:01:11 +0300

    Sample6Task done
    Still need to figure out what was wrong with the second XPath selector

--- src/selenium/sample/Sample6Task.java
+++ src/selenium/sample/Sample6Task.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
@@ -34,21 +35,48 @@ public class Sample6Task {
     public void findElementByXPath() throws Exception {
 //         TODO:
 //        2 ways to find text: "Heading 2 text":
+        WebElement elem = driver.findElement(By.xpath("//*[@id='heading_1']"));
+        System.out.println(elem.getText());
+        //elem = driver.findElement(By.xpath("/html/body/div[1]/h2[1]"));
+        //System.out.println(elem.getText());
 //        1-2 ways to find text: "Test Text 1"
+        elem = driver.findElement(By.xpath("/html/body/div[4]/p[1]"));
+        System.out.println(elem.getText());
 //        1-2 ways to find text: "Test Text 2"
+        elem = driver.findElement(By.xpath("/html/body/div[4]/p[2]"));
+        System.out.println(elem.getText());
 //        1-2 ways to find text: "Test Text 3"
+        elem = driver.findElement(By.xpath("/html/body/div[5]/p[1]"));
+        System.out.println(elem.getText());
 //        1-2 ways to find text: "Test Text 4"
+        elem = driver.findElement(By.xpath("/html/body/div[5]/p[1]"));
+        System.out.println(elem.getText());
 //        1-2 ways to find text: "Test Text 5"
+        elem = driver.findElement(By.xpath("/html/body/div[6]/p[1]"));
+        System.out.println(elem.getText());
 //        1-2 ways to find text: "This is also a button"
+        elem = driver.findElement(By.xpath("//*[@id=\"buttonId\"]"));
+        System.out.println(elem.getAttribute("value"));
+
     }
 
     @Test
     public void findElementByCssName() throws Exception {
 //         TODO:
 //        1-2 ways to find text: "Heading 2 text"
+        WebElement elem = driver.findElement(By.cssSelector("#heading_2"));
+        System.out.println(elem.getText());
 //        1-2 ways to find text: "Test Text 1"
+        elem = driver.findElement(By.cssSelector("#test1 > p:nth-child(1)"));
+        System.out.println(elem.getText());
 //        1-2 ways to find text: "Test Text 2"
+        elem = driver.findElement(By.cssSelector(".twoTest"));
+        System.out.println(elem.getText());
 //        1-2 ways to find text: "Test Text 3"
+        elem = driver.findElement(By.cssSelector("#test3 > p:nth-child(1)"));
+        System.out.println(elem.getText());
 //        1-2 ways to find text: "This is also a button"
+        elem = driver.findElement(By.cssSelector("#buttonId"));
+        System.out.println(elem.getAttribute("value"));
     }
 }
