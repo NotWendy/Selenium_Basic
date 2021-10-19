1367e08c094bb1ca14fb79f65e42fbc04e4fc96a
Amiran <47554589+NotWendy@users.noreply.github.com>
2021-10-19 15:23:01 +0300

    Sample8Task done

--- src/selenium/sample/Sample8Task.java
+++ src/selenium/sample/Sample8Task.java
@@ -40,5 +40,11 @@ public class Sample8Task {
 //        check the background of top 2 sections
 //        rgba(255, 221, 221, 1);
 //        check h1 element font-size 64px
+        WebElement div1 = driver.findElement(By.className("w3-pale-red"));
+        assertEquals("rgba(255, 221, 221, 1)", div1.getCssValue("background-color"));
+
+        WebElement div2 = driver.findElement(By.className("w3-pale-yellow"));
+        assertEquals("rgba(255, 255, 204, 1)", div2.getCssValue("background-color"));
+
     }
 }
