72b324b7822c4bad7e56b4ce94fb3415a387ca7e
Amiran <47554589+NotWendy@users.noreply.github.com>
2021-10-20 19:47:09 +0300

    Task2 done

--- src/selenium/tasks/Task2.java
+++ src/selenium/tasks/Task2.java
@@ -4,19 +4,26 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import selenium.pages.AgeSamplePage;
+import selenium.pages.FeedbackPage;
+import static org.junit.Assert.*;
 
 import java.io.File;
 
 public class Task2 {
-    WebDriver driver;
+    static WebDriver driver;
+    static FeedbackPage fPage;
 
     @Before
     public void openPage() {
         String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
         driver = new ChromeDriver();
-        driver.get("https://kristinek.github.io/sitetasks/provide_feedback");
+        driver.get("https://kristinek.github.io/site/tasks/provide_feedback");
+        fPage = PageFactory.initElements(driver, FeedbackPage.class);
     }
 
     @After
@@ -31,6 +38,8 @@ public class Task2 {
 //         "Don't know" is selected in "Genre"
 //         "Choose your option" in "How do you like us?"
 //         check that the button send is blue with white letters
+        fPage.checkFormClear();
+        fPage.checkInputButtonColors();
     }
 
     @Test
@@ -39,7 +48,12 @@ public class Task2 {
 //         click "Send" without entering any data
 //         check fields are empty or null
 //         check button colors
-//         (green with white letter and red with white letters)
+//         (green with white letters and red with white letters)
+        fPage.clickSubmit();
+        fPage.checkCheckPageClear();
+        WebElement yes = fPage.getCheckPageYes();
+        WebElement no = fPage.getCheckPageNo();
+        fPage.checkCheckPageButtonColors();
     }
 
     @Test
@@ -49,6 +63,9 @@ public class Task2 {
 //         check fields are filled correctly
 //         check button colors
 //         (green with white letter and red with white letters)
+        int[] langs = {0, 1};
+        fPage.submitAndVerifyForm("MY NAME", 30, langs, 0, "MY COMMENT");
+        fPage.checkCheckPageButtonColors();
     }
 
     @Test
@@ -59,6 +76,12 @@ public class Task2 {
 //         click "Yes"
 //         check message text: "Thank you, NAME, for your feedback!"
 //         color of text is white with green on the background
+        String name = "MY NAME";
+        fPage.setName(name);
+        fPage.clickSubmit();
+        fPage.getCheckPageYes().click();
+        fPage.checkThankYouMessage(name);
+        fPage.checkThankYouColors();
     }
 
     @Test
@@ -68,6 +91,10 @@ public class Task2 {
 //         click "Yes"
 //         check message text: "Thank you for your feedback!"
 //         color of text is white with green on the background
+        fPage.clickSubmit();
+        fPage.getCheckPageYes().click();
+        fPage.checkThankYouMessage("");
+        fPage.checkThankYouColors();
     }
 
     @Test
@@ -77,5 +104,11 @@ public class Task2 {
 //         click "Send"
 //         click "No"
 //         check fields are filled correctly
+        int[] langs = {0, 1};
+        fPage.submitForm("MY NAME", 30, langs, 0, "MY COMMENT");
+        fPage.getCheckPageNo().click();
+        fPage.refreshValues(driver);    //Refreshing the WebElement values, so that Selenium sees input fields again
+        fPage.checkFields("MY NAME", 30, langs, 0, "MY COMMENT");
     }
+
 }
